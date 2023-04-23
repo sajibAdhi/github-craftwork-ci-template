f1de28b2ece1bb73de1512d17dc4e1fd6b7fe487
Sajib Adhikary <60180521+sajibAdhi@users.noreply.github.com>
2023-04-23 17:49:07 +0000

    update index.test.js
    new test added

--- index.test.js
+++ index.test.js
@@ -4,3 +4,7 @@ test('that weekNum returns a value', () => {
   expect(weekNum).not.toBeNull();
 });
 
+// new test added
+test('that weekNum returns a number', () => {
+  expect(weekNum).toBeDefined();
+});
